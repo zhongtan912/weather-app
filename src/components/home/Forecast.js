8c37923602633ce4b85efac9c1877a58bf6bb745
alfie.tay <alfie.tay@accenture.com>
2020-07-07 14:51:42 +0800

    format

--- src/components/home/Forecast.js
+++ src/components/home/Forecast.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,7 +20,19 @@ function renderDetails(forecast) {
     for (let i = 0; i < 5; i++) {
         const curr = forecast[i];
         console.log(curr);
-        cards.push(<Grid key={i} item xs={3}><p>{(`${curr.day}/${curr.mintemp}/${curr.maxtemp}`)}</p></Grid>);
+        cards.push(
+            <Grid key={i} item xs>
+
+                <p>{curr.day}</p>
+                <span style={{ flex: '1 1 0%', textAlign: 'right' }}>
+                    <Typography variant="body2" component="span" color="textPrimary">
+                        {Math.round(curr.mintemp)}&deg; /{' '}
+                    </Typography>
+                    <Typography variant="body2" component="span" color="textSecondary">
+                        {Math.round(curr.maxtemp)}&deg;
+          </Typography>
+                </span>
+            </Grid>);
     }
 
     return cards;
