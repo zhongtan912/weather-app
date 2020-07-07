11fbd70a4fe798fc734b018af549a7fd5b9f0721
alfie.tay <alfie.tay@accenture.com>
2020-07-07 22:42:30 +0800

    header clean up

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,39 +1,55 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'flex-start'
+  },
+  menuButton: {
+    paddingLeft: '30px',
+    marginRight: theme.spacing(2),
+    alignSelf: 'flex-start'
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 const Header = () => {
-  return (
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    <AppBar position="static" style={{ marginBottom: '15px' }}>
-      <Tabs
-        variant="fullWidth"
-        aria-label="nav tabs example"
-      >
-        <Tab label="Home" to="/" component={Link} />
-        <Tab label="About" to="/about" component={Link} />
-      </Tabs>
-    </AppBar>
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" style={{ marginBottom: '15px' }}>
+        <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu" onClick={handleClick} >
+          <MenuIcon />
+        </IconButton>
+        <Menu id="nav-bar" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem to="/" component={Link} onClick={handleClose}>Home</MenuItem>
+          <MenuItem to="/about" component={Link} onClick={handleClose}>About</MenuItem>
+        </Menu>
+      </AppBar>
+    </div >
 
   );
 };
 
 export default Header;
-
-
-
-{/*
-    const activeStyle = { color: '#303f9f' };
-
-  <nav>
-<NavLink to="/" activeStyle={activeStyle} exact>
-  Home
-</NavLink>
-{' | '}
-<NavLink to="/about" activeStyle={activeStyle}>
-  About
-</NavLink>
-</nav> */}
